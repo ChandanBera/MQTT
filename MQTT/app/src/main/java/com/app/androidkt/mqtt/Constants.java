23bc8c1c24a4246a27103624f04c5288cbad9719
ChandanBera <chandankrbera@gmail.com>
2020-09-04 22:19:05 +0530

    first commit

---
+++ MQTT/app/src/main/java/com/app/androidkt/mqtt/Constants.java
@@ -0,0 +1,15 @@
+package com.app.androidkt.mqtt;
+
+/**
+ * Created by brijesh on 20/4/17.
+ */
+
+public class Constants {
+
+    public static final String MQTT_BROKER_URL = "tcp://54.161.195.215:1883";
+
+    public static final String PUBLISH_TOPIC = "topicMQTT";
+
+    public static final String CLIENT_ID = "androidkt"+System.currentTimeMillis();
+}
+
